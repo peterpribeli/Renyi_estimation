Working prototype. Fixed major bugs, added fitting and RMS calculation.

entropy.py
@@ -43,7 +43,7 @@ def uniformity_test(num_points, cent, rad, nseg):
         seg.append((rad*(i+1)/nseg)**(1./dim))
 
     for i in range(num_points):
-        point = np.subtract(get_point(cent, rad, i), cent)
+        point = np.subtract(get_point(cent, rad), cent)
         dist = 0.
         for j in range(dim):
             dist = dist + point[j]**2
@@ -58,15 +58,14 @@ def uniformity_test(num_points, cent, rad, nseg):
 
 def entropies(data, tree, alpha, radius, num_iter):
     """
-    Calculates the entropies for given alpha and radii. The zeroth element of alpha is corresponding to H1
-    and the first element to H_inf.
-    :param data: Array of input data.
+    Calculates the entropies for given alpha and radii. The zeroth element of alpha is corresponding to H_inf
+    and the first element to H_1.
+    :param data: List of input data.
     :param tree: The KDTree of the data.
-    :param center: An array of center point indices.
-    :param alpha: Array of alphas.
-    :param radius: Array of radii.
+    :param alpha: List of alphas.
+    :param radius: List of radii.
     :param num_iter: Number of iterations.
-    :return: 2-D array of tuples d[alpha][radius] = (entropy, k)
+    :return: Numpy array entropies[alp][rad]
     """
     m, _ = data.shape
     ml = np.log(m)
@@ -76,18 +75,16 @@ def entropies(data, tree, alpha, radius, num_iter):
     for ind in range(num_iter):
         for rad in range(len(radius)):
             # Get a random point from an n-ball around a random point
-            point = get_point(data[random.randrange(0, m), :], rad)
+            point = get_point(data[random.randrange(0, m), :], radius[rad])
 
             # Get the number of nearest neighbours
-            neigh = len(tree.query_ball_point(point, rad))+1
-
+            neigh = len(tree.query_ball_point(point, radius[rad]))+1
             for alp in range(len(alpha)):
                 # Skip alpha = 1
                 if alpha[alp] == 1.:
                     continue
                 # Calculate the degeneracy
                 entrop[alp+2][rad] += neigh**(alpha[alp]-1.)
-
 
             # Alpha = Infinity saved at entrop[0][:]
             entrop[0][rad] = max(entrop[0][rad], neigh)
@@ -96,14 +93,34 @@ def entropies(data, tree, alpha, radius, num_iter):
             entrop[1][rad] = entrop[1][rad] + np.log(neigh)
 
     # Rescale the degenerations to entropies
-    print(entrop)
     for rad in range(len(radius)):
         for alp in range(len(alpha)+2):
             if alp == 0.:
                 entrop[alp][rad] = ml - np.log(entrop[alp][rad])
             elif alp == 1.:
-                entrop[alp][rad] = ml - entrop[alp][rad]/(m*num_iter)
+                entrop[alp][rad] = ml - entrop[alp][rad]/num_iter
             else:
-                entrop[alp][rad] = ml + np.log(entrop[alp][rad]/(m*num_iter))/(1.-alpha[alp-2])
+                entrop[alp][rad] = ml + np.log(entrop[alp][rad]/num_iter)/(1.-alpha[alp-2])
 
     return entrop
+
+
+def get_rms(radius, entrop, pars):
+    """
+    Calculates the RMS of the fitted data.
+    :param radius: List of fitted radii
+    :param entrop: List of entropies[alpha][radius]
+    :param pars: List of the parameters of the linear fit
+    :return: List RMS[alpha]
+    """
+    m, n = entrop.shape
+    rms = []
+
+    for alp in range(m):
+        # Sum of squares of (data - prediction)**2
+        r = 0
+        for rad in range(n):
+            r += (entrop[alp][rad]-pars[0]*radius[rad]-pars[1])**2
+        rms.append(np.sqrt(r/n))
+
+    return rms

estimate.py
@@ -1,17 +1,18 @@
 import numpy as np
 from scipy import spatial
 from time import time
-from entropy import entropies
-from sklearn.linear_model import LinearRegression
+from entropy import entropies, get_rms
 import matplotlib.pyplot as plt
 
 
 if __name__ == "__main__":
 
     # Set input
-    num_iter = 10000
-    alpha = [0.05, 0.1, 0.8, 1.2, 1.9]
-    radius = [1e-5, 2e-5, 5e-5, 1e-4, 2e-4, 5e-4]
+    num_iter = 1000
+    # alpha = [0.05, 0.1, 0.8, 1.2, 1.9]
+    alpha = [0.8, 1.2]
+    radius = [1e-1, 2e-1, 3e-1, 5e-1, 6e-1, 7e-1, 8e-1, 9e-1, 1, 1.2, 1.5, 2, 3, 4, 5]
+    # radius = [5e-1, 7e-1, 8e-1, 9e-1, 1, 1.2, 1.5]
 
     # Import data
     data = np.genfromtxt('input1.csv', delimiter=' ')
@@ -20,19 +21,27 @@ if __name__ == "__main__":
     # Create k-d tree
     t0 = time()
     tree = spatial.cKDTree(data)
-    print("k-d tree construction took: {} s.".format(time()-t0))
+    print("k-d tree construction took: {:.1f} s.".format(time()-t0))
 
     # Calculate the entropies
     t0 = time()
     ent = entropies(data, tree, alpha, radius, num_iter)
-    print("Entropies calculation took: {} s.".format(time()-t0))
-    print(ent)
+    print("Entropies calculation took: {:.1f} s.".format(time()-t0))
 
     # Fit the data
-    reg = LinearRegression().fit(np.vstack((radius, ent[1, :])).T, np.vstack((radius, ent[1, :])).T)
-    print(reg.coef_)
+    strt = 4  # Begining of the fit
+    stp = 9  # End of the fit
+    lograd = [np.log(y) for y in radius]
+
+    coef, cov = np.polyfit(lograd[strt:stp], ent[2, strt:stp], 1, cov=True)
+    rms = get_rms(lograd[strt:stp], ent[:, strt:stp], coef)
+
+    print("The dimension is: {:.2f}, RMS: {:.2f}, sigma: {:.2f}".format(-coef[0], rms[2], np.sqrt(cov[0, 0])))
 
     # Plot the entropy vs radius for alpha=1
-    plt.plot(radius, ent[2, :])
-    plt.show()
+    x = np.linspace(radius[strt], radius[stp])
+    plt.xscale('log')
 
+    plt.plot(x, coef[0]*np.log(x)+coef[1], linestyle='solid', color='red')
+    plt.scatter(radius, ent[2, :])
+    plt.show()